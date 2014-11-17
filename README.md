f1f68cdc3f6c9b6808a703808350a3802f080ea1
jason <jasonzliang>
2014-11-17 17:40:53 -0600

    finished up report

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ autoencoder
 ===========
 
 A Stacked Autoencoder that is Trained via Parallelized Evolutionary Optimization
+
+todo:
+-cache encoded inputs and corrupted inputs
+-implement fine-tuning
+-use different weight initialization
+-cross-entrophy loss for autoencoder
--- src/main.cpp
+++ src/main.cpp
@@ -68,13 +68,13 @@ void train_and_test_network_square(int numOuterIter, vector<int> &trainLabels, f
 void train_and_test_autoencoder(vector<int> &trainLabels, float **trainingImages, vector<int> &testLabels, float **testingImages)
 {
   vector<int> autoencoder_layers {784, 1000, 1000};
-  vector<float> auto_learn_rates {0.0015, 0.0015, 0.0015};
-  vector<int> auto_iters {10, 10, 10};
+  vector<float> auto_learn_rates {0.001, 0.001, 0.001};
+  vector<int> auto_iters {15, 15, 15};
   vector<float> noise_levels {0.1, 0.2, 0.3};
 
   autoencoder *myAutoencoder = new autoencoder(autoencoder_layers, auto_learn_rates, auto_iters, noise_levels, 1000, 500, 10, 0.1);
   myAutoencoder->preTrain(trainingImages, numTrainingImages);
-  myAutoencoder->train(trainingImages, trainLabels, 30, numTrainingImages);
+  myAutoencoder->train(trainingImages, trainLabels, 40, numTrainingImages);
   myAutoencoder->test(testingImages, testLabels, numTestingImages);
   delete myAutoencoder;
 }
@@ -178,10 +178,10 @@ int main(int argc, char *argv[])
 
   // train_and_test_network_square(30, training_labels, training_images, testing_labels, testing_images);
   // train_and_test_network_cross(30, training_labels, training_images, testing_labels, testing_images);
-  // train_and_test_autoencoder(training_labels, training_images, testing_labels, testing_images);
+  train_and_test_autoencoder(training_labels, training_images, testing_labels, testing_images);
   // experiment_1(training_labels, training_images);
   // experiment_2(training_labels, training_images);
-  experiment_3(training_labels, training_images, testing_labels, testing_images);
+  // experiment_3(training_labels, training_images, testing_labels, testing_images);
 
   for (int i = 0; i < numTrainingImages; i++)
   {
